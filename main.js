1bb0b636c047f959f6a1537a0718fe327cffb425
yujin Park <parkyu@adenasoft.com>
2021-02-08 23:48:55 +0900

    이제시작

---
+++ main.js
@@ -0,0 +1,16 @@
+
+//복합연산자를 사용한 경우
+    let list = "";
+    list += '<ul>'
+    list += '<li>hello</li>'
+    list += '<li>Javasctipt</li>'
+    list += '</ul>'
+
+    document.write(list);
+
+/* 복합연산자를 사용 안 한 경우 
+    // const list = '<ul><li>hello</li><li>javascript</li></ul>';
+    // document.write(list);
+
+    // document.write('<ul><li>hello</li><li>javascript</li></ul>');
+*/
